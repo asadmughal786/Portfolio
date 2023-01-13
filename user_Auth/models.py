bbd14bcfd2ba37bd8d21ae4d2c6268722466f0a9
Mohammad Asad <Asad Mughal@ThinkPad-T460s>
2023-01-13 15:02:55 +0500

    User registerations Models and Model Forms Done

--- user_Auth/models.py
+++ user_Auth/models.py
@@ -11,7 +11,12 @@ class user_resgisteration(models.Model):
     user_city = models.CharField(max_length=15,null=False)
     user_freelancer = models.BooleanField()
     user_bio = models.TextField(max_length=255,null=False)
-    user_profile_picture = models.ImageField(null=True)
+    user_profile_picture = models.ImageField(upload_to='image/',default=None)
     user_webstie = models.URLField()
     user_password = models.CharField(max_length=8)
     user_conf_password = models.CharField(max_length=8)
+
+class user_images(models.Model):
+    user = models.OneToOneField(user_resgisteration,on_delete=models.CASCADE,null=True,blank=True)
+    save_date = models.DateTimeField()
+    
